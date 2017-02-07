438e7cde6988c6d51bc1f6840b5a47cee2bc5da6
Tech-A34 <Tech-A34@rich-0023.uk.brad-ex.co.uk>
2017-02-07 11:17:54 +0000

    updated

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 //declaring the routes on the app.js
-const main = require('./routes/sessLkn');
-const users = require('./routes/users');
+const sessLkn = require('./routes/sessLkn');
 
 const app = express();
 
@@ -18,8 +16,6 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,10 +25,7 @@ app.use(session({secret: "code",
                 saveUninitialized: false,
                 resave: true}));
 
-
-// app.use(index);
-app.use('/', main);
-app.use('/users', users);
+app.use('/', sessLkn);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,31 +4,6 @@ class IndexController{
     static index(req,res){
         res.render("view");
     }
-//static method that gets the results for the object of linkedin info
-    static linkedData(req, res){
-        Grab.grabDaStuff()
-            .then(results => {
-                res.status(200).send({
-                    results: results
-                });
-            })
-            .catch(err =>{
-                res.status(400).send(err.message);
-            });
-        }
-//creates a session in order to host the linkedin object data
-    static createSessionStore(req, res){
-        Grab.sessionStuff()
-            .then(results => {
-                req.session.data = results;
-                res.status(200).send({
-                    results: results
-                });
-            })
-            .catch(err =>{
-                res.status(400).send(err.message);
-            });
-    }
 }
 
 
